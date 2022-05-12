8289d6219843864427e6d691ec87f901b080d57d
Nazmul <nazmulislamn95@gmail.com>
2022-05-12 18:31:50 +0600

    JS Template literal

---
+++ style.js
@@ -0,0 +1,10 @@
+
+var a = 5;
+var b = 6;
+
+var c = a + b;
+
+console.log("The summation of " + a + " + " + b + " is ", c);
+//Template Literals - Modern JavaScript Syntaxes
+console.log(`The summation of ${a} + ${b} is : ${c}`);
+console.log(`Summation is ${a + b}`);
